16af4cdb34979033b7000d3e0fea39b6d1553fd1
一块蒙脱石 <montmorillonite@qq.com>
2023-08-08 08:04:19 +0800

    :bug: 修复了 `unescape_unicode` 无法去转义的问题

--- src/plugins/echo.py
+++ src/plugins/echo.py
@@ -4,11 +4,7 @@ from nonebot import on_command
 from nonebot.matcher import Matcher
 from nonebot.params import CommandArg
 from nonebot.permission import SUPERUSER
-from nonebot.adapters.onebot.v11 import (
-    Message,
-    MessageEvent,
-    unescape as unescape_CQcode
-)
+from nonebot.adapters.onebot.v11 import Message, MessageEvent, unescape
 
 from ._lang import parse
 from .github import unescape_emoji
@@ -20,8 +16,7 @@ async def echo_handler(
     event: MessageEvent,
     arg: Message = CommandArg()
 ) -> None:
-    arg = str(arg)
-    arg = unescape_CQcode(arg)
+    arg = unescape(str(arg))
     arg = parse(arg, event)
     arg = unescape_unicode(arg)
     arg = unescape_emoji(arg)
@@ -30,6 +25,6 @@ async def echo_handler(
 
 def unescape_unicode(string: str) -> str:
     return re.sub(
-        pattern=r"u+[0-9a-f]", string=string.lower(),
+        r"u\+[0-9a-f]+", string=string.lower(),
         repl=lambda match: chr(int(match.group()[2:], base=16))
     )
